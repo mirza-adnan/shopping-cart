1cc4c9a7aaebc586ef14db295ae0d967a3542d02
mirza-adnan <mirza.adnan2205@gmail.com>
2021-08-03 23:12:28 +0600

    added global styles

---
+++ src/components/styles/GlobalStyles.style.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    :root {
+        --clr-primary: #24252a;
+        --clr-text: #EEEEEE;
+        --clr-accent: #00ADB5;
+    }
+
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        height: 100%;
+    }
+
+    body {
+        height: 100%;
+        font-family: "Montserrat", sans-serif;
+        background-color: var(--clr-primary);
+    }
+`;
+
+export default GlobalStyles;
