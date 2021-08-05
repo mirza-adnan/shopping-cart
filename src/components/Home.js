9ea95f3b8b1b4fd1849bd4976412a0bfebbf3194
mirza-adnan <mirza.adnan2205@gmail.com>
2021-08-05 19:53:15 +0600

    designed the homepage

---
+++ src/components/Home.js
@@ -0,0 +1,81 @@
+import React from "react";
+import styled from "styled-components";
+import shopping from "./assets/img/shopping.svg";
+import wave from "./assets/img/wave.svg";
+
+function Home() {
+    return (
+        <HomeContainer>
+            <Wrapper>
+                <HomeText>
+                    <h1>
+                        Bringing you <span>tomorrows</span> fashion{" "}
+                        <span>today</span>
+                    </h1>
+                </HomeText>
+                <HomeImage>
+                    <img src={shopping} alt="" />
+                </HomeImage>
+            </Wrapper>
+            <Wave src={wave} />
+        </HomeContainer>
+    );
+}
+
+const HomeContainer = styled.main`
+    flex: 1;
+    position: relative;
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    padding: 2rem 3rem;
+
+    @media (max-width: 1020px) {
+        flex-direction: column;
+        gap: 2rem;
+    }
+`;
+
+const HomeText = styled.div`
+    width: 50%;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    font-size: clamp(1.5rem, 3vw, 2.5rem);
+
+    & span {
+        color: var(--clr-accent);
+    }
+
+    @media (max-width: 1020px) {
+        width: 100%;
+        justify-content: center;
+        text-align: center;
+    }
+`;
+const HomeImage = styled.div`
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem 1rem;
+
+    & img {
+        width: 100%;
+        max-width: 580px;
+    }
+
+    @media (max-width: 1020px) {
+        width: 100%;
+        height: 0%;
+    }
+`;
+
+const Wave = styled.img`
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+`;
+
+export default Home;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,28 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import styled from "styled-components";
 import Header from "./Header";
+import Home from "./Home";
 
 function Main() {
     return (
-        <BrowserRouter>
-            <Header />
-        </BrowserRouter>
+        <Container>
+            <BrowserRouter>
+                <Header />
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </Container>
     );
 }
+
+const Container = styled.div`
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+`;
 
 export default Main;
