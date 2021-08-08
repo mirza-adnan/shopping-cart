fb879f40d8e53d1405c0221465ffca81ebb713ff
mirza-adnan <mirza.adnan2205@gmail.com>
2021-08-08 22:14:17 +0600

    fixed typo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import GlobalStyles from "./components/styles/GlobalStyles.style";
+import GlobalStyle from "./components/styles/GlobalStyle.style";
 
 ReactDOM.render(
     <React.StrictMode>
-        <GlobalStyles />
+        <GlobalStyle />
         <App />
     </React.StrictMode>,
     document.getElementById("root")
